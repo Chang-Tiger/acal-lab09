56b1920f6398262704408971332b232161e5129d
Tiger <chtiger1@gmail.com>
2024-08-14 21:10:09 +0800

    Add new project to <group version>

--- Hardware/Lab/src/main/resource/inst.asm
+++ Hardware/Lab/src/main/resource/inst.asm
@@ -1,43 +1,211 @@
-addi x16, x0, 0xfec
-addi x17, x0, 0x472
-addi x18, x0, 0xab7
-addi x19, x0, 0xda2
-addi x20, x0, 0x561
-addi x21, x0, 0x146
-addi x22, x0, 0x531
-addi x23, x0, 0x456
-addi x24, x0, 0x11
-addi x25, x0, 0x98
-addi x26, x0, 0xa3
-addi x27, x0, 0x10
-addi x28, x0, 0x55
-addi x29, x0, 0xffe
-addi x30, x0, 0x4
-addi x31, x0, 0xff1
-clz x1, x30
-cpop x2, x23
-xnor x3, x16, x17
-min x4, x31, x25
-maxu x5, x19, x30
-sext.b x6, x21
-bseti x7, x29, 0x0
-bclr x8, x31, x30
-binv x9, x16, x24
-bexti x10, x21, 0x2
-rol x11, x16, x30
-rori x12, x23, 0xc
-sh2add x13, x17, x18
-rev8 x14, x25
-orc.b x15, x25
-nop zero, zero, 0
-nop zero, zero, 0
-nop zero, zero, 0
-nop zero, zero, 0
-nop zero, zero, 0
-hcf
-nop
-nop
-nop
-nop
-nop
+li ra,16
+nop 
+nop 
+nop 
+nop 
+nop 
+slli sp,ra,0x3
+nop 
+nop 
+nop 
+nop 
+nop 
+xori gp,sp,-2048
+nop 
+nop 
+nop 
+nop 
+nop 
+slti tp,sp,-3
+nop 
+nop 
+nop 
+nop 
+nop 
+sltiu t0,sp,-3
+nop 
+nop 
+nop 
+nop 
+nop 
+srli t1,gp,0x2
+nop 
+nop 
+nop 
+nop 
+nop 
+srai t2,gp,0x2
+nop 
+nop 
+nop 
+nop 
+nop 
+ori s0,zero,123
+nop 
+nop 
+nop 
+nop 
+nop 
+andi s1,t0,-1
+nop 
+nop 
+nop 
+nop 
+nop 
+add a0,t0,t1
+nop 
+nop 
+nop 
+nop 
+nop 
+sub a1,gp,t2
+nop 
+nop 
+nop 
+nop 
+nop 
+sll a2,t0,s0
+nop 
+nop 
+nop 
+nop 
+nop 
+slt a3,t2,s1
+nop 
+nop 
+nop 
+nop 
+nop 
+sltu a4,s1,sp
+nop 
+nop 
+nop 
+nop 
+nop 
+xor a5,a1,gp
+nop 
+nop 
+nop 
+nop 
+nop 
+srl a6,a3,tp
+nop 
+nop 
+nop 
+nop 
+nop 
+sra a7,a5,t0
+nop 
+nop 
+nop 
+nop 
+nop 
+or s2,a7,ra
+nop 
+nop 
+nop 
+nop 
+nop 
+and s3,a3,a0
+nop 
+nop 
+nop 
+nop 
+nop 
+sb gp,3(zero)
+nop 
+nop 
+nop 
+nop 
+nop 
+sw ra,4(zero)
+nop 
+nop 
+nop 
+nop 
+nop 
+lb s4,3(zero)
+nop 
+nop 
+nop 
+nop 
+nop 
+lw s5,4(zero)
+nop 
+nop 
+nop 
+nop 
+nop 
+lbu s6,3(zero)
+nop 
+nop 
+nop 
+nop 
+nop 
+lui s8,0x1238
+nop 
+nop 
+nop 
+nop 
+nop 
+beq ra,sp,<hello>
+nop 
+nop 
+nop 
+nop 
+nop 
+bne ra,ra,<hello>
+nop 
+nop 
+nop 
+nop 
+nop 
+bltz ra,<hello>
+nop 
+nop 
+nop 
+nop 
+nop 
+bgez gp,<hello>
+nop 
+nop 
+nop 
+nop 
+nop 
+bltu a1,gp,<hello>
+nop 
+nop 
+nop 
+nop 
+nop 
+bgeu gp,a1,<hello>
+nop 
+nop 
+nop 
+nop 
+nop 
+jalr s9,752(zero)
+nop 
+nop 
+nop 
+nop 
+nop 
+add a0,t0,t1
+nop 
+nop 
+nop 
+nop 
+nop 
+j <exit>
+nop 
+nop 
+nop 
+nop 
+nop 
+sll a2,t0,s0
+nop 
+nop 
+nop 
+nop 
+nop 
 hcf
